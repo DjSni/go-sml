0a376a1b44fe0a22b95c82495e99d2176c7b3a5b
DjSni <31524614+DjSni@users.noreply.github.com>
2026-09-25 13:56:41 +0200

    Vendor Pulse parser test fixture

--- pulse_test.go
+++ pulse_test.go
@@ -3,18 +3,26 @@ package sml
 import (
 	"bufio"
 	"bytes"
+	"encoding/hex"
 	"os"
+	"strings"
 	"testing"
 )
 
 func pulseFrame(t *testing.T) []byte {
 	t.Helper()
-	data, err := os.ReadFile(`S:\node_data.bin`)
+	hexData, err := os.ReadFile("testdata/node_data.hex")
+	if err != nil {
+		t.Fatal(err)
+	}
+	data, err := hex.DecodeString(strings.TrimSpace(string(hexData)))
 	if err != nil {
 		t.Fatal(err)
 	}
 	return data
 }
+
+const nodeDataHex = "1B1B1B1B0101010176050DE7EB4C62006200726301017601010504A2A3C40B0A0149534B000516465C7262016504A2A20862016335990076050DE7EB4D620062007263070177010B0A0149534B000516465C070100620AFFFF7262016504A2A20875770701004032010101010101010449534B0177070100600100FF010101010B0A0149534B000516465C0177070100010800FF650008010401621E52FF6503FEC26A0177070100020800FF0101621E52FF62000177070100100700FF0101621B520053017F01010163BB0E0076050DE7EB4E6200620072630201710163F1A9001B1B1B1B1A00F61F"
 
 func TestPulseFrameParsesThreeMessagesWithCRC(t *testing.T) {
 	frame := pulseFrame(t)
